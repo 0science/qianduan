7701cb1a77e8b1f3602ec9df2d6a9cd773fb78a6
djm <1415847612@qq.com>
2024-11-19 23:56:24 +0800

    新增内容，vue实战

---
+++ 3.VUE/vite/src/components/Demo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{msg}}</div>
+</template>
+
+<script>
+// 模块导出
+export default{
+  //setup组合式api
+  setup(){
+    return {
+      // 返回对象
+    msg :'不积跬步无以至千里'
+  }
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ 3.VUE/vite/src/components/RefMessage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>{{ msg }}</div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+let msg = ref('不积跬步,无以至千里') 
+
+setTimeout(() => {
+    console.log("更新前", msg.value)
+    msg.value = '不积小流,无以成江海'
+    console.log("更新后", msg.value)
+}, 2000)
+
+</script>
+
+<style></style>
